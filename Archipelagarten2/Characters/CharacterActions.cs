9026709d8988e43060c55edfc409deb2553712dc
Alex Gilbert <alexgilbert@yahoo.com>
2024-07-23 19:07:59 -0400

    - Try to walk to the player before killing him

--- Archipelagarten2/Characters/CharacterActions.cs
+++ Archipelagarten2/Characters/CharacterActions.cs
@@ -1,9 +1,14 @@
-﻿using KG2;
+﻿using DG.Tweening;
+using KG2;
+using System;
+using UnityEngine;
 
 namespace Archipelagarten2.Characters
 {
     public class CharacterActions
-    { 
+    {
+        private const float DISTANCE_TO_BE_VISIBLE = 2f;
+
         public void MoveNPCToCurrentRoom(NPCBehavior npc)
         {
             var npcRoom = npc.GetCurrentRoom();
@@ -17,6 +22,22 @@ namespace Archipelagarten2.Characters
             npc.player.SetPlayerState(PlayerState.AnimState);
             EnvironmentController.Instance.SetCharacterRoom(npc.transform, playerRoom, npcRoom);
             npc.SetCameraTarget();
+
+            var currentDistance = Math.Abs(npc.transform.position.x - npc.player.transform.position.x) +
+                                  Math.Abs(npc.transform.position.y - npc.player.transform.position.y);
+
+            if (currentDistance < 3)
+            {
+                return;
+            }
+
+            var num = -DISTANCE_TO_BE_VISIBLE;
+            if (npc.transform.position.x > (double)npc.player.transform.position.x)
+            {
+                num = DISTANCE_TO_BE_VISIBLE;
+            }
+
+            npc.WalkStraightLine(new Vector3(npc.player.transform.localPosition.x + num, npc.player.transform.localPosition.y, npc.player.transform.localPosition.z), 0.0f, npc.FacePlayer);
         }
     }
 }
